a24f712c9b608d335f0730bd1a1d58882e1a8117
Jofleming <jordano.fleming@gmail.com>
2022-04-25 18:38:42 -0700

    finished lab 4

---
+++ README.md
@@ -0,0 +1,8 @@
+# LAB - Pythonic Garage Band
+## Author - Jordan Fleming
+
+This project is to use python classes to model a band, with custom musicians.
+
+### Tests
+Pytest is used to run tests.
+Tests were supplied.
---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,53 @@
+class Band():
+    instances = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+
+    def play_solos(self):
+        solos_list = []
+        for member in self.members:
+            solos_list.append(member.play_solo())
+        return solos_list
+    
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+
+class Musician():
+    instrument = ''
+    title = ''
+    solo = ''
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f'My name is {self.name} and I play {self.instrument}'
+
+    def __repr__(self):
+        return f'{self.title} instance. Name = {self.name}'
+
+    def get_instrument(self):
+        return f'{self.instrument}'
+
+    def play_solo(self):
+        return f'{self.solo}'
+
+class Guitarist(Musician):
+    instrument = 'guitar'
+    title = 'Guitarist'
+    solo = 'face melting guitar solo'
+
+class Drummer(Musician):
+    instrument = 'drums'
+    title = 'Drummer'
+    solo = 'rattle boom crash'
+
+class Bassist(Musician):
+    instrument = 'bass'
+    title = 'Bassist'
+    solo = 'bom bom buh bom'
